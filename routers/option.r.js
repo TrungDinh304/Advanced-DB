7857c10f10df50a2950f5e56693571fa15354f62
NHThang2k3 <118982196+NHThang2k3@users.noreply.github.com>
2024-01-04 10:25:56 +0700

    Delete routers directory

--- routers/option.r.js
+++
@@ -1,8 +0,0 @@
-const app = require("express");
-const router = app.Router();
-
-const optionController = require('../controllers/option.c');
-router.get("/", optionController.renderOption);
-
-
-module.exports = router;
